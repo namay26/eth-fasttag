da0e6012da5981912a4be20fb0bd0e32eec93f7b
namay26 <namayrohatgi@gmail.com>
2024-12-07 02:30:47 +0530

    Add routing

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,6 @@
 import { registerRootComponent } from 'expo';
 
 
-import Home from './src/components/Home';
-import Fastag from './src/components/Fastag';
-import Page1 from './src/components/Page1';
-import Page2 from './src/components/Page2';
-import Profile from './src/components/Profile';
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(Profile);
+import App from './App';
+
+registerRootComponent(App);
