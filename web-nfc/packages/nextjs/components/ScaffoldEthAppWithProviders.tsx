52ffbe848dd30a8ed2501dcf55f4ca6d820ce71d
namay26 <namayrohatgi@gmail.com>
2024-12-07 18:27:25 +0530

    Add footer header logic

--- web-nfc/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ web-nfc/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -12,18 +12,19 @@ import { Header } from "~~/components/Header";
 import { BlockieAvatar } from "~~/components/scaffold-eth";
 import { useInitializeNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
 import { wagmiConfig } from "~~/services/web3/wagmiConfig";
-import { usePathname } from "next/navigation";  // Import the usePathname hook
+import { usePathname } from "next/navigation"; // Import the usePathname hook
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   useInitializeNativeCurrencyPrice();
   const pathname = usePathname(); // Get the current pathname
 
-  const showHeaderFooter = !pathname.startsWith("/"); // Hide header/footer on /reader page
+  // Only show header and footer if the pathname is not "/"
+  const showHeaderFooter = pathname !== "/"; // Hide header/footer on '/' page
 
   return (
     <>
       {showHeaderFooter && (
-        <div className={`flex flex-col min-h-screen `}>
+        <div className={`flex flex-col min-h-screen`}>
           <Header />
           <main className="relative flex flex-col flex-1">{children}</main>
           <Footer />
